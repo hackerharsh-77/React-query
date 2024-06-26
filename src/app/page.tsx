be7138d487facd4c0e43096f64c29d7f7936b1bd
hackerharsh-77 <r.harsh0177@gmail.com>
2024-06-26 23:57:33 +0530

    chore: added error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,49 @@
 "use client"
 
 import { useQuery } from "react-query";
+import axios from "axios";
 
 
-const useTodo = (id: number) =>{
- const {isLoading, isError, data} = useQuery(['TODOS',id], ()=>{
-    return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(response => response.json())
-  })
-  return {isLoading, isError, todo: data}
+const usePost = (id: number) =>{
+//  const {isLoading, isError, data, error} = useQuery(['POSTS',id], ()=>{
+//     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//       .then(response => {
+//         if(!response.ok){
+//           throw new Error('Post not found !')
+//         }
+//         return response.json();
+//       })
+//   }, {
+//     retry: false,
+//     refetchOnWindowFocus: false
+//   })
+const {isLoading, isError, data, error} = useQuery(['POSTS',id], ()=>{
+  return axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then(response => response.data)
+}, {
+  retry: false,
+  refetchOnWindowFocus: false
+})
+
+  return {isLoading, isError, post: data, error: error as Error}
 }
 
 export default function Home() {
   
-  const {isLoading,todo} = useTodo(1);
+  const {isLoading,isError,post, error} = usePost(11);
 
   if(isLoading) return <h1>Loading...</h1>
+  if(isError) return <h1>Error: {error.message}</h1>
 
-  console.log(todo);
+  console.log(post);
+  
   
   return (
     <div>
       <h1>Hello world</h1>
-      <h2>Task: #{todo.id}</h2>
-      <h2>Title: {todo.title}</h2>
-      <h2>User: {todo.userId}</h2>
-      <h2>Is completed? : {todo.completed}</h2> 
+      <h2>Task: #{post.id}</h2>
+      <h2>Title: {post.title}</h2>
+      <h2>Description: {post.body}</h2>
     </div>
   );
 }
